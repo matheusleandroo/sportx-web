3c51012a8047f96c357e11de128a3ba67d3d6b78
Matheus Leandro <mattleandroo@outlook.com>
2020-08-16 10:58:33 -0300

    add dotenv

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const apiURL = process.env.REACT_APP_API_URL;
+
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: apiURL,
 });
 
 export default api;
